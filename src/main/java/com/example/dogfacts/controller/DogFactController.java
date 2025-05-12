dfb123fd2eeeb24a4b70eaae203447d623bbdb2d
Ngô Quang Tiến <quangtienngo661@gmail.com>
2025-05-13 00:29:26 +0700

    complete the connection between frontend and backend, display information on view

--- src/main/java/com/example/dogfacts/controller/DogFactController.java
+++ src/main/java/com/example/dogfacts/controller/DogFactController.java
@@ -4,7 +4,9 @@ import com.example.dogfacts.service.DogFactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -14,6 +16,7 @@ public class DogFactController {
     @Autowired
     private DogFactService dogFactService;
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500") // Cho phép CORS cho endpoint này
     @GetMapping("/")
     public String showDogFacts(Model model) {
         List<String> facts = dogFactService.getDogFacts();
